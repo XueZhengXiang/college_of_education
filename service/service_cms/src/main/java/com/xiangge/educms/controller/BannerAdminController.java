b8fb143b7412c5d01817dc9c28f187d3d9496ee6
xiangGe <2235915272@qq.com>
2022-08-02 22:19:16 +0800

    前台轮播图

--- service/service_cms/src/main/java/com/xiangge/educms/controller/BannerAdminController.java
+++ service/service_cms/src/main/java/com/xiangge/educms/controller/BannerAdminController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.*;
  * <p>
  * 首页banner表 前端控制器
  * </p>
- *
- * @author testjava
- * @since 2022-07-20
  */
 @RestController
 @RequestMapping("/educms/banneradmin")
